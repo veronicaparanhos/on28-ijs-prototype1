5f83dfd9e36250ff7b2ca126a305b1624d236fc1
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-09-23 12:32:15 -0300

    resolução exercício 4

--- exercicios/para-sala/exercicio-4/index.js
+++ exercicios/para-sala/exercicio-4/index.js
@@ -7,3 +7,13 @@ employee1.addBenefits('Zenklub');
 employee1.listBenefits();
 employee1.removeBenefits('VA');
 employee1.listBenefits();
+employee1.raiseSalary(0.05);
+
+const employee2 = Employee('Maria', 'Santos', 10000);
+employee2.addBenefits('VR');
+employee2.addBenefits('VA');
+employee2.addBenefits('Zenklub');
+employee2.listBenefits();
+employee2.removeBenefits('VA');
+employee2.listBenefits();
+employee2.raiseSalary(0.05);
